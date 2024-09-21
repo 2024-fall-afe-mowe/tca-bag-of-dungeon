08509ca10c25450cc4a095f6211581d1db2061c8
CEBlank <96022511+CEBlank@users.noreply.github.com>
2024-09-21 17:22:11 -0500

    Initial test
    Testing tailwind and daisyUI layouts.

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="form-control flex items-center">
       <h1
-      className='text-3xl font-bold underline'
+      className='text-2xl font-bold underline mb-4'
       >
         Bag of Dungeon Companion App</h1>
       <button className="btn btn-primary">Primary</button>
+    </div>
     </div>
   );
 }
